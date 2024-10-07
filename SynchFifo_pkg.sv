d33ab4898129629773b90a6067782065954d48af
Tarek Rayan <tarekrayyan50@gmail.com>
2024-10-07 21:59:18 +0300

    Testbench Development in progress

--- SynchFifo_pkg.sv
+++ SynchFifo_pkg.sv
@@ -4,7 +4,12 @@ package SynchFifo_pkg;
     `include "uvm_macros.svh";
     `include "sequence_item.svh";
     `include "sequencer.svh";
+    `include "base_sequence.svh";
+    `include "monitor.svh";
     `include "agent.svh";
+    `include "coverage.svh";
     `include "env.svh";
     `include "base_test.svh";
+    
+    
 endpackage
