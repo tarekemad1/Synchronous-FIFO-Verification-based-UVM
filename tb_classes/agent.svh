7b8fc96215fd11f217ba0181f2f62f4f86ed3838
Tarek Rayan <tarekrayyan50@gmail.com>
2024-10-07 22:01:47 +0300

    Testbench Development in progress

--- tb_classes/agent.svh
+++ tb_classes/agent.svh
@@ -1,11 +1,13 @@
 class agent extends uvm_agent;
     `uvm_component_utils(agent);
     sequencer sequencer_h; 
+    monitor monitor_h ; 
     function new(string name , uvm_component parent );
         super.new(name , parent); 
     endfunction
     function void build_phase(uvm_phase phase );
         sequencer_h = sequencer::type_id::create("sequencer_h",this);
+        monitor_h = monitor::type_id::create("monitor_h",this);
 
     endfunction
 endclass
--- tb_classes/env.svh
+++ tb_classes/env.svh
@@ -1,11 +1,13 @@
 class env extends uvm_env;
     `uvm_component_utils(env);
     agent agent_h ; 
+    cvrge coverage_h ; 
     function new(string name  , uvm_component parent);
         super.new(name,parent);
     endfunction
     function void build_phase(uvm_phase phase );
         super.build_phase(phase);
         agent_h = agent::type_id::create("agent_h",this);
+        coverage_h = cvrge::type_id::create("coverage_h",this);
     endfunction
 endclass
