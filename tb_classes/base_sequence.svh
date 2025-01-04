c98f6044a1bae665b08b54744f07bd4c8e91567c
Tarek Rayan <91722282+tarekemad1@users.noreply.github.com>
2025-01-04 13:40:29 +0200

    Delete tb_classes directory

--- tb_classes/base_sequence.svh
+++
@@ -1,7 +0,0 @@
-class base_sequence extends uvm_sequence#(sequence_item);
-    `uvm_object_utils(base_sequence);
-    function new(string name = "base_sequence");
-        super.new(name );
-    endfunction
-
-endclass
